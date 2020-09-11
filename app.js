2e72521898fb90c964a3fdccf7f3881b4b775e6a
Michael Gobz <47690301+michaelgobz@users.noreply.github.com>
2020-09-11 15:41:26 +0300

    making_routes work

--- app.js
+++ app.js
@@ -17,6 +17,44 @@ app.get('/dashboard', (req, res) => {
     res.render('dashboard')
 });
 
+app.get('/all_appointments', (req, res) => {
+    res.render('all_appointments')
+});
+
+app.get('/all_patients', (req, res) => {
+    res.render('all_patients')
+});
+app.get('/logout', (req, res) => {
+    res.render('login')
+});
+app.get('/transactions', (req, res) => {
+    res.render('transactions')
+});
+app.get('/all_employees', (req, res) => {
+    res.render('all_employees')
+});
+app.get('/profile', (req, res) => {
+    res.render('profile')
+})
+app.get('/mail_compose', (req, res) => {
+    res.render('mail_compose')
+});
+
+app.get('/mail_detail', (req, res) => {
+    res.render('mail_detail')
+});
+
+app.get('/employee_profiles', (req, res) => {
+    res.render('employee_profiles')
+});
+
+app.get('/mailbox', (req, res) => {
+    res.render('mailbox')
+});
+app.get('/register', (req, res) => {
+    res.render('register')
+});
+
 app.listen(port, () => {
     console.log(`server running on ${port}`);
 });
